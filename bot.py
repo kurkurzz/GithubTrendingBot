6cb1648c1a4826b9394070e44b17dd0e04ddd5dd
Muhammad Nurhafiz <64152220+kurkurzz@users.noreply.github.com>
2020-12-09 21:39:18 +0800

    share and close

--- bot.py
+++ bot.py
@@ -6,15 +6,14 @@ from credentials import TOKEN
 client = discord.Client()
 
 @client.event
-async def on_message(message):
-    if message.author!=client.user:
-        for guild in client.guilds:
-            channel =  get(guild.text_channels,name='💌share-your-tech')
-            if channel is not None:
-                top_repo = get_top_repo()
-                display_text = f'Top GitHub repo for today is **{top_repo.name}** by **{top_repo.author}**' \
-                    f'Link: {top_repo.url}'
-                await channel.send(display_text)
-
+async def on_ready():
+    for guild in client.guilds:
+        channel =  get(guild.text_channels,name='💌share-your-tech')
+        if channel is not None:
+            top_repo = get_top_repo()
+            display_text = f'Top GitHub repo for today is **{top_repo.name}** by **{top_repo.author}**' \
+                f'\nLink: {top_repo.url}'
+            await channel.send(display_text)
+            await client.close()
 
 client.run(TOKEN)
